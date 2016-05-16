1773b2de69a5bb47d1bc92789befa466a776c8a2
ruijie <ruijie@kanjoya.com>
2016-05-16 10:46:32 -0700

    removed view2, add contact folder, inital set up, intall more dependencies

---
+++ app/contacts/contacts.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('mycontacts.contacts', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/contacts', {
+    templateUrl: 'contacts/contacts.html',
+    controller: 'contactsCtrl'
+  });
+}])
+
+.controller('contactsCtrl', [function() {
+	console.log("the contacts controller is loading");
+}]);
