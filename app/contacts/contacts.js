05b54ad7055fb42a8ebf89a732ff7929c121346f
ruijie <ruijie@kanjoya.com>
2016-05-22 13:29:56 -0700

    all functionalities implemented

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -12,6 +12,22 @@ angular.module('mycontacts.contacts', ['ngRoute','firebase','mycontacts'])
 .controller('contactsCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
     var ref = new Firebase('https://mycontacts-rz.firebaseio.com/');
     $scope.contacts = $firebaseArray(ref);
+
+    $scope.showContact = false;
+    $scope.showContactForm = function(contact) {
+            console.log(contact);
+            $scope.showContact = true;
+            $scope.name = contact.name;
+            $scope.email = contact.email;
+            $scope.company = contact.company;
+            $scope.home_phone = contact.phones[0].home;
+            $scope.mobile_phone = contact.phones[0].mobile;
+            $scope.work_phone = contact.phones[0].work;
+            $scope.zipcode = contact.address[0].zipcode;
+            $scope.city = contact.address[0].city;
+            $scope.state = contact.address[0].state;
+            $scope.street_address = contact.address[0].street_address;
+    }
     
     // set the flag addForm to be true, and thus show the form used to add contact
     $scope.showAddForm = function() {
@@ -20,6 +36,27 @@ angular.module('mycontacts.contacts', ['ngRoute','firebase','mycontacts'])
 
     $scope.hideAddForm = function() {
         $scope.addForm = false;
+    }
+
+    var clearFields = function() {
+        $scope.name = "";
+        $scope.email = "";
+        $scope.company = "";
+        $scope.mobile_phone = "";
+        $scope.home_phone = "";
+        $scope.work_phone = "";
+        $scope.street_address = "";
+        $scope.city = "";
+        $scope.state = "";
+        $scope.zipcode = "";
+    }
+
+    $scope.removeContact = function(contact){
+        console.log('Removing Contact');
+
+        $scope.contacts.$remove(contact);
+
+        $scope.msg="Contact Removed";
     }
 
     $scope.addFormSubmit = function() {
@@ -65,6 +102,56 @@ angular.module('mycontacts.contacts', ['ngRoute','firebase','mycontacts'])
             $scope.addForm = false;
 
         })
-
     }
+
+    $scope.showEditForm = function(contact){
+        $scope.editForm= true;
+
+        $scope.id               = contact.$id;
+        $scope.name             = contact.name;
+        $scope.email            = contact.email;
+        $scope.company          = contact.company;
+        $scope.work_phone       = contact.phones[0].work;
+        $scope.home_phone       = contact.phones[0].home;
+        $scope.mobile_phone     = contact.phones[0].mobile;
+        $scope.street_address   = contact.address[0].street_address;
+        $scope.city             = contact.address[0].city;
+        $scope.state            = contact.address[0].state;
+        $scope.zipcode          = contact.address[0].zipcode;
+    }
+
+
+    $scope.editFormSubmit = function() {
+        console.log('updating contact');
+
+        // get ID
+        var id = $scope.id;
+
+        var record = $scope.contacts.$getRecord(id);
+
+        record.name                         = $scope.name;
+        record.email                        = $scope.email;
+        record.company                      = $scope.company;
+        record.phones[0].work               = $scope.work_phone;
+        record.phones[0].home               = $scope.home_phone;
+        record.phones[0].mobile             = $scope.mobile_phone;
+        record.address[0].street_address    = $scope.street_address;
+        record.address[0].city              = $scope.city;
+        record.address[0].state             = $scope.state;
+        record.address[0].zipcode           = $scope.zipcode;
+
+        // Save Conrtact
+        $scope.contacts.$save(record).then(function(ref){
+            console.log(ref.key);
+        });
+
+        clearFields();
+
+        // Hide Form
+        $scope.editFormShow = false;
+
+        $scope.msg = "Contact Updated";
+    }
+
+    
 }]);
